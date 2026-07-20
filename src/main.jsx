002a8650c8461784c1684eb1705d860924b5f412
MartinForgetProfessionnal <107161274+MartinForgetProfessionnal@users.noreply.github.com>
2026-07-20 14:10:31 -0400

    chore(frontend): force new asset hashes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx';
 import './index.css';
 import { CartProvider } from './CartContext.jsx';
 
+const FLIPPINMAPLE_BUILD_CACHE_BUSTER = '20260720-chrome-module-cache';
+void FLIPPINMAPLE_BUILD_CACHE_BUSTER;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
