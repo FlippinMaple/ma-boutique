a1830385e940e70204e9f5771f0515bbfd87b68a
MartinForgetProfessionnal <107161274+MartinForgetProfessionnal@users.noreply.github.com>
2026-07-28 08:47:04 -0400

    fix(api): rate limit Printful inventory checks

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -1,6 +1,7 @@
 // routes/inventoryRoutes.js
 import { Router } from 'express';
 import { getPrintfulStock } from '../controllers/inventoryController.js';
+import { inventoryLimiter } from '../middlewares/rateLimiters.js';
 const router = Router();
-router.get('/printful-stock/:id', getPrintfulStock);
+router.get('/printful-stock/:id', inventoryLimiter, getPrintfulStock);
 export default router;
